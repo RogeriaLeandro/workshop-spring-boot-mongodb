6f58c8b6803cdc6a99b4c4f50354c61245ad360f
RogeriaLeandro <rogeriaaraujoleandro@gmail.com>
2021-05-13 14:49:02 -0300

    Conectando ao MongoDB com Repository e Service

--- src/main/java/com/rogerialeandro/workshopmongo/domain/User.java
+++ src/main/java/com/rogerialeandro/workshopmongo/domain/User.java
@@ -2,10 +2,17 @@ package com.rogerialeandro.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private Integer id;
 	private String name;
 	private String email;
